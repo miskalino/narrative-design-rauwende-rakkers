0a9f6cb59dc450b0f2fbb9bdc86ab0096a4f7cd4
miskalino <miskatalo@hotmail.com>
2023-01-31 09:01:14 +0100

    code quit name change

--- Assets/Scenes/scripts/Menu.cs
+++ Assets/Scenes/scripts/Menu.cs
@@ -10,7 +10,7 @@ public class Menu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void Quit()
+    public void QuitGame()
     {
         Application.Quit();
     }
